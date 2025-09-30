b00bb3cc941ae2f777d8a4b161c32f15c2d79b3c
Kitki30 <kitki30@proton.me>
2025-09-30 17:42:37 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+
+from flask import Flask, redirect, send_file, make_response
+
+app = Flask(__name__)
+
+BASE_DIR = os.path.dirname(__file__)
+
+urls = {
+    "github": "https://github.com/kitki30",
+    "stick": "https://github.com/stickfirmware",
+    "stick_repo": "https://github.com/stickfirmware/stick/",
+    "device_table": "https://github.com/stickfirmware/stick-tutorials/blob/main/install/table.md",
+    "tutorials": "https://www.youtube.com/playlist?list=PLTu2NSjIK0c7TVbUVAlP5KkjpFISeaogt",
+    "esptool": "https://espressif.github.io/esptool-js/",
+    "mpydownloads": "https://micropython.org/download/",
+    "thonnydownloads": "https://thonny.org/",
+    "trello": "https://trello.com/b/ZT1qrlDO/stick-firmware",
+    "docs": "https://docs.kitki30.tk",
+    "myip": "https://api.kitki30.tk/ip",
+    "converter": "https://convert.kitki30.tk/"
+}
+
+@app.route('/<code>')
+def short_redirect(code):
+    target = urls.get(code)
+    if target:
+        return redirect(target)
+    
+    # 404 page
+    path = os.path.join(BASE_DIR, "pages", "err_invalid.html")
+    if not os.path.isfile(path):
+        return "404 Not Found", 404
+
+    response = make_response(send_file(path))
+    response.status_code = 404
+    return response
+
+@app.route('/')
+def main():
+    return redirect("https://www.kitki30.tk/")
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=6767)
